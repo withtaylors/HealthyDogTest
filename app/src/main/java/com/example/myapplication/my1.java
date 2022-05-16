ee43975bd25d57efd5c21f0fad3330f9f1bc8575
heart <heartsally0707@gmail.com>
2022-05-16 17:26:09 +0900

    5.16

---
+++ app/src/main/java/com/example/myapplication/my1.java
@@ -0,0 +1,113 @@
+package com.example.myapplication;
+
+import static com.example.myapplication.R.array;
+import static com.example.myapplication.R.id;
+import static com.example.myapplication.R.layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class my1 extends AppCompatActivity {
+
+
+        ArrayAdapter<CharSequence> adspin1, adspin2, adspin3, adspin4, adspin5;
+        Button Female_1;
+        Button Male_1;
+private boolean check = false;
+        ImageButton profile_pic1;
+
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = uiOptions;
+        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
+        if (isImmersiveModeEnabled) {
+        Log.i("Is on?", "Turning immersive mode mode off. ");
+        } else {
+        Log.i("Is on?", "Turning immersive mode mode on.");
+        }
+        newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+
+
+        setContentView(layout.activity_my1);
+final Spinner spin1 = (Spinner) findViewById(id.spinner_year1);
+final Spinner spin2 = (Spinner) findViewById(id.spinner_month1);
+final Spinner spin3 = (Spinner) findViewById(id.spinner_day1);
+final Spinner spin4 = (Spinner) findViewById(id.spinner_type1);
+final Spinner spin5 = (Spinner) findViewById(id.spinner_gender1);
+
+        adspin1 = ArrayAdapter.createFromResource(this, array.year, android.R.layout.simple_spinner_dropdown_item);
+        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(adspin1);
+
+        adspin2 = ArrayAdapter.createFromResource(this, array.month, android.R.layout.simple_spinner_dropdown_item);
+        adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin2.setAdapter(adspin2);
+
+        adspin3 = ArrayAdapter.createFromResource(this, array.day, android.R.layout.simple_spinner_dropdown_item);
+        adspin3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin3.setAdapter(adspin3);
+
+        adspin4 = ArrayAdapter.createFromResource(this, array.type, android.R.layout.simple_spinner_dropdown_item);
+        adspin4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin4.setAdapter(adspin4);
+
+        adspin5 = ArrayAdapter.createFromResource(this, array.gender, android.R.layout.simple_spinner_dropdown_item);
+        adspin5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin5.setAdapter(adspin5);
+
+        Female_1 = (Button) findViewById(id.Female_1);
+        Female_1.setOnClickListener(new OnClickListener() {
+@Override
+public void onClick(View view) {
+        if (check == false) {
+        check = true;
+        Female_1.setSelected(true);
+        } else {
+        check = false;
+        Female_1.setSelected(false);
+        }
+        }
+        });
+
+        Male_1 = (Button) findViewById(id.Male_1);
+        Male_1.setOnClickListener(new OnClickListener() {
+@Override
+public void onClick(View view) {
+        if (check == false) {
+        check = true;
+        Male_1.setSelected(true);
+        } else {
+        check = false;
+        Male_1.setSelected(false);
+        }
+        }
+        });
+
+        ImageButton profile_pic1 = (ImageButton) findViewById(id.profile_pic1);
+        profile_pic1.setOnClickListener(new View.OnClickListener() {
+
+
+@Override
+public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), mypop1.class);
+        startActivity(intent);
+        }
+
+        });
+        }
+        }
---
+++ app/src/main/java/com/example/myapplication/mypop1.java
@@ -0,0 +1,105 @@
+package com.example.myapplication;
+
+import static android.content.Intent.ACTION_GET_CONTENT;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mypop1 extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int CROP_FROM_IMAGE = 2;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypopup);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+
+        layoutParams.dimAmount = 0.5f;
+        getWindow().setAttributes(layoutParams);
+
+
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+        int width = (int) (display.getWidth() * 0.25);
+        int height = (int) (display.getHeight() * 0.15);
+        getWindow().getAttributes().width = width;
+        getWindow().getAttributes().height = height;
+
+        Button gallery = (Button) findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+
+
+        Button camera = (Button) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+
+                startActivityForResult(intent, PICK_FROM_CAMERA);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK)
+            return;
+        switch (requestCode) {
+            case PICK_IMAGE_REQUEST: {
+
+
+
+            }
+
+
+            case PICK_FROM_CAMERA: {
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.putExtra("outputX", 100);
+                intent.putExtra("outputY", 100);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_IMAGE);
+                break;
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
