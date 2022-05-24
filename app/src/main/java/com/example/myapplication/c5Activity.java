dcc8839c86b22fca6a8fc8326ae94509cd2cde95
sucharmi <iji8399@naver.com>
2022-05-25 06:12:03 +0900

    suji_220525

--- app/src/main/java/com/example/myapplication/c5Activity.java
+++ app/src/main/java/com/example/myapplication/c5Activity.java
@@ -2,7 +2,14 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +31,23 @@ public class c5Activity extends AppCompatActivity {
 
         result.setText(sub_result);
         confidences.setText(sub_confidences);
+
+        //infoText 중간 글씨 색 바꾸기
+        TextView function_text = (TextView)findViewById(R.id.infoText); //텍스트 변수 선언
+        String content = function_text.getText().toString(); //텍스트 가져옴.
+        SpannableString spannableString = new SpannableString(content); //객체 생성
+
+        //infoText 속 특정 문자(눈 혼탁 증상이 있을 확률)의 시작위치 끝위치 얻기
+        String word ="눈 혼탁 증상이 있을 확률";
+        int start = content.indexOf(word);
+        int end = start + word.length();
+
+        //spannableString 속성 정하기
+        spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#E77794")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        //spannableString.setSpan(new StyleSpan(Typeface.BOLD), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        // spannableString.setSpan(new RelativeSizeSpan(1.3f), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        function_text.setText(spannableString);
 
     }
 }
